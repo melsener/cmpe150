ce20b584966d05312b8afb3bc65295e5f33a4d02
melsener <melisasener@gmail.com>
2019-02-22 22:33:27 +0300

    week2 added

---
+++ week2_extra_1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	// C is case sensitive. Be careful for your variable names.
+	int x = 3;
+	int X = 8;
+	printf("%d %d",x,X);
+	return 0;
+}
---
+++ week2_extra_2.c
@@ -0,0 +1,28 @@
+/*
+ * 
+ *
+ *  Created on: Feb 20, 2019
+ *      Author: melisaMac
+ */
+
+#include <stdio.h>
+#include <limits.h>
+
+int main(){
+	// An integer can not hold TOO BIG numbers.
+	int a = INT_MAX;
+	printf("a = %d\n",a);
+
+	int b = a + 1;
+	printf("b = %d\n",b);
+
+	//To keep bigger numbers use "long" data type.
+	long c = a;
+	c = c + 1;
+	printf("c = %ld\n",c);
+
+	//Notice this
+	long d = a + 1;
+	printf("d = %ld\n",d);
+
+}
