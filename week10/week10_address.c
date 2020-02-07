8b7be56f8bf00d2fec8feb7093dc1f4a8e809e80
melsener <melisasener@gmail.com>
2020-02-07 22:16:00 +0300

    deleted last year

--- week10/week10_address.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	int a[3];
-	printf("%p:array\n%p:&array[0]\n%p:&array\n",a,&a[0],&a);
-	return 0;
-}
--- week2/week2_extra_1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	// C is case sensitive. Be careful for your variable names.
-	int x = 3;
-	int X = 8;
-	printf("%d %d",x,X);
-	return 0;
-}
